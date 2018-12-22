947d85925ee01288172def3f8ba6dbf921992d1f
Mr.Jay <40586590+jayagami@users.noreply.github.com>
2018-12-22 14:48:45 +0800

    forget to add remove xtu config script

---
+++ remove_xtu_config.ps1
@@ -0,0 +1,12 @@
+﻿# remove xtu config
+Set-PSDebug -Off
+Unregister-ScheduledJob -Name xtus -Confirm:$false |Out-Null
+Unregister-ScheduledJob -Name xtu -Confirm:$false |Out-Null
+Unregister-ScheduledTask -TaskName volt -Confirm:$false |Out-Null
+Unregister-ScheduledTask -TaskName watt -Confirm:$false |Out-Null
+Unregister-ScheduledTask -TaskName bostwatt -Confirm:$false |Out-Null
+Unregister-ScheduledTask -TaskName bosttime -Confirm:$false |Out-Null
+
+Write-Host "Removed!"
+
+sleep 3
